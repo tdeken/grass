45fdc26c592185be83ec824ee68d768132d8611d
eken <eken@branchcn.com>
2024-09-09 19:03:55 +0800

    bug fix

--- action/create_handler.go
+++ action/create_handler.go
@@ -239,7 +239,7 @@ func (s *createHandler) file(parses []basic.Parse) (err error) {
 				return
 			}
 			var str = bytes.NewBuffer(b)
-			if strings.Contains(str.String(), fmt.Sprintf("%s(ctx *fiber.Ctx)", v1.Name)) {
+			if strings.Contains(str.String(), fmt.Sprintf("%s(%s)", v1.Name, s.getCtxTemp())) {
 				continue
 			}
 
